dc8d1370afa6b3d33196d57e936d12e66a8bb92b
Najam <hmnajam@gmail.com>
2023-08-28 21:26:46 +0500

    vision tool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,6 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
-// import {visionTool} from '@sanity/vision'
+import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
