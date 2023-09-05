10cf03018adfbe4c039636d1ec3676e1b559820f
Najam <hmnajam@gmail.com>
2023-09-05 20:56:06 +0500

    updating ip code.

--- src/app/api/testing/route.ts
+++ src/app/api/testing/route.ts
@@ -4,11 +4,14 @@ export async function GET(request: NextRequest) {
   const url = request.nextUrl;
 
   if (url.searchParams.has("name")) {
-    const ip = request.ip;
+  //  const ip = request.ip;
+    const ip =
+    request.headers.get("x-forwarded-for") || request.connection.remoteAddress;
+
     const name = url.searchParams.get("name");
     const age = url.searchParams.get("age");
     const currentTime = new Date();
-    return new NextResponse(`Hello! I am ${name}. I am ${age} years old. 
+    return new NextResponse(`Hello! I am ${name}. I am ${age} years old.
     My IP address is ${ip}. The time right now is ${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`);
   } else {
     return new NextResponse('Please send your name in search parameter "name" and age in "age"');
