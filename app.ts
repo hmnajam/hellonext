b3039568e31fe3eeb696040027ea32d24bb2d948
hmnajam <hmnajam@gmail.com>
2023-09-19 19:49:53 +0500

    commented whole app.ts

--- app.ts
+++ app.ts
@@ -1,19 +1,19 @@
-import { OpenAI } from "langchain/llms/openai";
-import 'dotenv/config';
+// import { OpenAI } from "langchain/llms/openai";
+// import 'dotenv/config';
 
 
-console.log(process.env.OPENAI_API_KEY);
+// console.log(process.env.OPENAI_API_KEY);
 
-const llm = new OpenAI({
- openAIApiKey: process.env.OPENAI_API_KEY,
- temperature: 0.9,
-});
+// const llm = new OpenAI({
+//  openAIApiKey: process.env.OPENAI_API_KEY,
+//  temperature: 0.9,
+// });
 
 
-async function main() {
- const result = await llm.predict(`What would be a good company name for a company that makes colorful socks?`);
- // Handle the result...
- console.log(result)
-}
+// async function main() {
+//  const result = await llm.predict(`What would be a good company name for a company that makes colorful socks?`);
+//  // Handle the result...
+//  console.log(result)
+// }
 
-main();
+// main();
